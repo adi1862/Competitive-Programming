cffa64ae7b8fca7b74eb9ff21b59b2e331e369e9
adi1862 <33635849+adi1862@users.noreply.github.com>
2019-06-19 23:32:29 +0530

    Update Coin Change problem.cpp
    comment added for better understanding

--- Coin Change problem.cpp
+++ Coin Change problem.cpp
@@ -1,5 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+/*---------------------------Coin Change Probelm--------------------------------------------------
+
+The idea behind this algorithm is to create a combination array that will contain the combination of the coin of given denomination
+corresponding to each index, index is actually denoting the value that we want to get using these coins.  
+
+we will take coins one by one of given denomination then we will first check if the coin value is less than the amount(index)
+i.e if(inddex >= coin[i])
+
+if th above combination is true then simply update the corresponding value of combination 
+i.e combination[index] += combination[index - coin[i]];
+
+*/
+
+
 int coinChange(vector<int> coins,int amount){
     int com[amount+3];
     memset(com,0,sizeof(com));
